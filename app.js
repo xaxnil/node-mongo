4c434ea4f1e4b165de717020b9cff3c073e5c893
leticia <lettax_05@hotmail.com>
2020-06-16 01:41:38 -0600

    HTTPS

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ connect.then(() => console.log('Connected correctly to server'),
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -47,7 +57,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/partners', partnerRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
-
 
 
 // catch 404 and forward to error handler
