184b406db22e4742cb9b1b7fd5701a73b062e0b5
leticia <lettax_05@hotmail.com>
2020-06-19 01:36:20 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '697604891061516',
+        clientSecret: 'e4f0617bc3818b97a34fba59569c9afc'
+    }
 }
